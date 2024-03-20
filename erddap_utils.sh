62cbc01b5213832d3882bb6bb8bcda2cc85d2586
JaredMclellan <jaredamclellan@gmail.com>
2024-03-20 14:44:53 +0000

    Moved utility files out of utils folder

--- erddap_utils.sh
+++ erddap_utils.sh
@@ -1,6 +1,5 @@
-export ERDDAP_DIR=opt/applications/docker-erddap
+export ERDDAP_DIR=/opt/applications/docker-erddap
 
 generate_datasetsd(){
-    echo ${ERDDAP_DIR}
     sudo bash ${ERDDAP_DIR}/datasets.d.sh -d ${ERDDAP_DIR}/erddap/datasets.d -o ${ERDDAP_DIR}/erddap/content/datasets.xml -w
 }
